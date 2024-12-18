5d3a741d61347b98e5845388f5e17d21cbcf4118
CaioSalchesttes <caiosalchesttes@hotmail.com>
2024-12-18 19:56:41 -0300

    1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Unisinos</title>
-    <link rel="icon" type="image/x-icon" href="assets/images/unisinos.png">
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
     @vite('resources/css/app.css')
 </head>
 <style>
